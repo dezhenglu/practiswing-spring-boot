57d9c4c4cc6c909fb2690bbca991077395a4ea1e
irof <irof@hogedriven.net>
2017-01-15 22:23:58 +0900

    エラーメッセージを変更

--- alfa-web/src/main/java/alfa/spring/security/WebSecurityConfig.java
+++ alfa-web/src/main/java/alfa/spring/security/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package alfa.spring.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,15 +30,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().logoutSuccessUrl("/?logout");
     }
 
-    /**
-     * PasswordEncoderをBean定義したら自動的に使われる。
-     * 定義しなかったら PlaintextPasswordEncoder になる模様。
-     *
-     * @return encoder
-     * @see org.springframework.security.authentication.encoding.PlaintextPasswordEncoder
-     */
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    void configureAuthenticationManager(AuthenticationManagerBuilder builder,
+                                        UserDetailsService service) throws Exception {
+        builder.userDetailsService(service)
+                .passwordEncoder(passwordEncoder());
     }
 }
