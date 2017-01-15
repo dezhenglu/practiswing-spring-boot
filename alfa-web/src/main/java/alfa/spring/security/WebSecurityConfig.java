21c8a4738290b9132ff313fa97cf2c50579018e3
irof <irof@hogedriven.net>
2017-01-15 22:25:06 +0900

    いらないので削除

--- alfa-web/src/main/java/alfa/spring/security/WebSecurityConfig.java
+++ alfa-web/src/main/java/alfa/spring/security/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package alfa.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 /**
  * @author irof
  */
-@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
