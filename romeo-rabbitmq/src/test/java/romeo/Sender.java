2b49abd2a098491c38b439786d2a7c1a27b796ae
irof <irof@hogedriven.net>
2017-08-09 08:32:54 +0900

    amqpのgetting started

---
+++ romeo-rabbitmq/src/test/java/romeo/Sender.java
@@ -0,0 +1,31 @@
+package romeo;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+import org.junit.Test;
+
+import java.time.LocalTime;
+import java.util.Collections;
+
+public class Sender {
+
+    @Test
+    public void test() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+
+        try (Connection conn = factory.newConnection()) {
+            Channel channel = conn.createChannel();
+            try (AutoCloseable ac = channel::close) {
+                String QUEUE = "romeo.in";
+
+                channel.queueDeclare(QUEUE, false, false, false, Collections.emptyMap());
+                byte[] body = ("HELLO_" + LocalTime.now()).getBytes();
+                channel.basicPublish("neko", QUEUE, MessageProperties.TEXT_PLAIN, body);
+            }
+        }
+    }
+}
