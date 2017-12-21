ffc5d39b52f4fee572953e9fe2c570a9456b98c7
irof <irof@hogedriven.net>
2017-12-22 02:18:16 +0900

    starter-webだけのやつ

---
+++ whiskey-web/src/main/java/whiskey/WhiskeyApplication.java
@@ -0,0 +1,12 @@
+package whiskey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiskeyApplication.class, args);
+    }
+}
